abafa3662ea1dc3a3387546afec9322ce969af90
rinsuki <428rinsuki+git@gmail.com>
2018-01-25 04:38:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# server proxy
+
+指定された TCP ポートで接続を待ち受け、接続されたら指定されたホストとポートに指定された HTTP プロキシを通じてコネクションを確立します。
+Node.js製です。
+
+## how to use
+
+```
+server-tcp-proxy 3000 localhost:8080 example.com:80
+```
+
+この場合、`3000`ポートで待ち受け、HTTPプロキシ`localhost:8080`を経由して`example.com:80`に接続します。
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import * as net from "net"
+
+const LISTEN_PORT = process.argv[2]
+const HTTP_PROXY = process.argv[3].split(":")
+const REMOTE_HOST = process.argv[4]
+
+const server = net.createServer(function(con) {
+    const client = net.createConnection({
+        host: HTTP_PROXY[0],
+        port: parseInt(HTTP_PROXY[1])
+    }, () => {
+        client.write("CONNECT "+REMOTE_HOST+" HTTP/1.1\n\n")
+        var connected = false
+        client.on("data", data => {
+            if (!connected) {
+                console.log(data.toString("utf-8"))
+                con.pipe(client)
+                client.pipe(con)
+                connected = true
+            }
+        })
+        client.on("close", () => {
+            con.end()
+        })
+        con.on("close", () => {
+            con.end()
+        })
+        client.on("error", (err) => {
+            con.destroy(err)
+        })
+        con.on("error", (err) => {
+            client.destroy(err)
+        })
+    })
+})
+
+server.listen(LISTEN_PORT)
